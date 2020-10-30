97b20665b01cb5834dfdd8d3dc40eafe324093f4
Abby Howe <howe.abigail@gmail.com>
2020-10-29 20:37:58 -0500

    * .gitignore: Initial Commit
    * Program.cs:
    * Startup.cs:
    * appsettings.json:
    * site.js:
    * favicon.ico:
    * site.css:
    * _ViewStart.cshtml:
    * Index.cshtml:
    * ErrorViewModel.cs:
    * _ViewImports.cshtml:
    * Privacy.cshtml:
    * Error.cshtml:
    * _Layout.cshtml:
    * appsettings.Development.json:
    * LICENSE:
    * HomeController.cs:
    * LICENSE.txt:
    * launchSettings.json:
    * 10.6._Studio_SkillsTracker.sln:
    * SkillsController.cs:
    * 10.6._Studio_SkillsTracker.csproj:
    * jquery.js:
    * jquery.min.js:
    * jquery.min.map:
    * LICENSE.md:
    * bootstrap.js:
    * bootstrap.css:
    * _ValidationScriptsPartial.cshtml:
    * bootstrap.min.js:
    * bootstrap.js.map:
    * bootstrap.css.map:
    * bootstrap.min.css:
    * bootstrap-grid.css:
    * bootstrap.bundle.js:
    * bootstrap.min.js.map:
    * bootstrap-reboot.css:
    * bootstrap.min.css.map:
    * LICENSE.txt:
    * bootstrap.bundle.js.map:
    * bootstrap.bundle.min.js:
    * jquery.validate.js:
    * bootstrap-grid.css.map:
    * bootstrap-grid.min.css:
    * bootstrap-reboot.css.map:
    * bootstrap-reboot.min.css:
    * additional-methods.js:
    * bootstrap.bundle.min.js.map:
    * jquery.validate.min.js:
    * bootstrap-grid.min.css.map:
    * bootstrap-reboot.min.css.map:
    * additional-methods.min.js:
    * jquery.validate.unobtrusive.js:
    * jquery.validate.unobtrusive.min.js:

---
+++ Controllers/SkillsController.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace _10._6._Studio_SkillsTracker.Controllers
+{
+    public class SkillsController : Controller
+    {
+        // GET: /<controller>/
+        [HttpGet]
+        [HttpPost("/skills/form")]
+        [Route("/skills")]
+        public IActionResult Index(string date, string cSelect, string javaSelect, string javascriptSelect)
+        {
+            string html = "<h1>Skills Tracker:" + date + "</h1>" +
+                "<h2>Coding Skills to Learn</h2>" +
+                "<h3>" +
+                "<ol>1.  C#:  " + cSelect + "</ol>" +
+                "<ol>2.  Java:  " + javaSelect + "</ol>" +
+                "<ol>3.  JavaScript:  " + javascriptSelect + "</ol>" +
+                "</h3>";
+            return Content(html, "text/html");
+        }
+        // GET: /skills/form
+        [HttpGet]
+        //[HttpPost("/skills/form")]
+        public IActionResult Form()
+        {
+            string html = "<form method='post' action='/skills/form'>" +
+                    "<label>" +
+                        "Date:" +
+                        "<input type='date' name ='date'>" +
+                    "</label>" +
+                    "<label>" +
+                        "C#:" +
+                    "</label>" +
+                    "<select name= 'cSelect'>" +
+                        "<option value='Learning The Basics'>Learning The Basics</option>" +
+                        "<option value='Creating Apps'>Creating Apps</option>" +
+                        "<option value='Master Coder'>Master Coder</option>" +
+                    "</select>" +
+                    "<label>" +
+                    "Java:" +
+                    "</label>" +
+                    "<select name= 'javaSelect'>" +
+                        "<option value='Learning The Basics'>Learning The Basics</option>" +
+                        "<option value='Creating Apps'>Creating Apps</option>" +
+                        "<option value='Master Coder'>Master Coder</option>" +
+                    "</select>" +
+                    "<label>" +
+                    "JavaScript:" +
+                    "</label>" +
+                    "<select name= 'javascriptSelect'>" +
+                        "<option value='Learning The Basics'>Learning The Basics</option>" +
+                        "<option value='Creating Apps'>Creating Apps</option>" +
+                        "<option value='Master Coder'>Master Coder</option>" +
+                    "</select>" +
+                    "<input type='submit' value='Submit' />" + 
+                   "</form>";
+            return Content(html, "text/html"); 
+        }
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace _10._6._Studio_SkillsTracker.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
